11a519e0de28c671d7a7427cb841944f7651a91d
TNO-TheMemer <71560042+TNO-TheMemer@users.noreply.github.com>
2020-11-24 00:24:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from pySmartDL import SmartDL
 
 
 ######################################################################################
-bot_token ='YOUR_BOT_TOKEN_HERE'       #
+bot_token ='1210665584:AAEDaAxqm1DGn_MviYZVXhtW6cbYBoj3OEQ'       #
 
 updater = Updater(token= bot_token, use_context=True)                                #
 
